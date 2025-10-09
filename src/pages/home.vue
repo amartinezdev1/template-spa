f476a189ad7ea59ac518e83376f719dd63f81e47
amartinezdev1 <angelmarti18@icloud.com>
2025-10-09 07:06:59 -0500

    deploy project

---
+++ src/pages/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <nav class="d-flex justify-space-between align-center">
+            <div>
+                <v-img
+                    :width="125"
+                    cover
+                    src="https://preview.robertbiswas.com/jekyll/lezar/assets/images/logos/logo.png"
+                ></v-img>
+            </div>
+            <ul class="d-flex ga-10">
+                <li><a href="">Home</a></li>
+                <li><a href="">Pages</a></li>
+                <li><a href="">Portfolio</a></li>
+                <li><a href="">Service</a></li>
+                <li><a href="">Blog</a></li>
+            </ul>
+            <div class="d-flex ga-5">
+                <v-icon icon="$vuetify" size="large"></v-icon>
+                <v-icon icon="$vuetify" size="large"></v-icon>
+                <v-icon icon="$vuetify" size="large"></v-icon>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script setup></script>
